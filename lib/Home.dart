bea5722948506d1693e16c8f8baceb7e603b2ad6
Gabriel Menoi <ga.gabrielmenoi@gmail.com>
2021-10-26 15:01:42 -0300

    primeiro commit

---
+++ lib/Home.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  TextEditingController _controllerAlcool = TextEditingController();
+  TextEditingController _controllerGasolina = TextEditingController();
+  String _textoResultado = "";
+
+
+  void _calcular(){
+
+    double? precoAlcool = double.tryParse( _controllerAlcool.text );
+    double? precoGasolina = double.tryParse( _controllerGasolina.text );
+
+    if( precoAlcool == null || precoGasolina == null ){
+      setState(() {
+        _textoResultado = "Número inválido, digite números maiores que 0 e utilizando (.) ";
+      });
+    }else{
+      if( (precoAlcool / precoGasolina) >= 0.7 ){
+        setState(() {
+          _textoResultado = "Resultado: Melhor abastecer com Gasolina";
+        });
+      }else{
+        setState(() {
+          _textoResultado = "Resultado: Melhor abastecer com Álcool";
+        });
+      }
+
+      //_limparCampos();
+
+    }
+
+
+  }
+
+  void _limparCampos(){
+    _controllerGasolina.text = "";
+    _controllerAlcool.text = "";
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Álcool ou Gasolina ?"),
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: Container(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(32),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(bottom: 32),
+                child: Image.asset("imagens/imagemHome.png"),
+              ),
+              Padding(
+                padding: EdgeInsets.only(bottom: 10),
+                child: Text(
+                  "Saiba qual a melhor opção de combustivel para o seu carro no momento.",
+                  style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Insira o Preço Alcool, ex: 4.20"
+                ),
+                style: TextStyle(
+                    fontSize: 22
+                ),
+                controller: _controllerAlcool,
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Insira o Preço Gasolina, ex: 5.50"
+                ),
+                style: TextStyle(
+                    fontSize: 22
+                ),
+                controller: _controllerGasolina,
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 10),
+                child: RaisedButton(
+                    color: Colors.blueAccent,
+                    textColor: Colors.white,
+                    padding: EdgeInsets.all(15),
+                    child: Text(
+                      "Calcular",
+                      style: TextStyle(
+                          fontSize: 20
+                      ),
+                    ),
+                    onPressed: _calcular
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: Text(
+                  _textoResultado,
+                  style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
